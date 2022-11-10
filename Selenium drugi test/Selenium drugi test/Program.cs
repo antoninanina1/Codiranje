d7359733f2a19c04d046e53316b0c02a8d6ff809
QA36 <QA36@DESKTOP-QPQFVHR>
2022-11-10 20:47:20 +0100

    init

---
+++ Selenium drugi test/Selenium drugi test/Program.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_drugi_test
+{
+    public class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+           
+
+
+        }
+
+        [SetUp]
+        public void Instalacija()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebElement username = driver.FindElement(By.Id("user-name"));
+            username.SendKeys("standard_user");
+
+            IWebElement password = driver.FindElement(By.Id("password"));
+            password.SendKeys("secret_sauce");
+
+            IWebElement LoginButton = driver.FindElement(By.Id("login-button"));
+
+            LoginButton.Submit();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Close();
+        }
+
+    }
+}
